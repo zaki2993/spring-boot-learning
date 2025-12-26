50503ac2cffb3cff11a2d740022394716e865f9f
zaki <zakaprog@gmail.com>
2025-12-26 13:42:35 +0100

    setters injection

--- src/main/java/zaki/example/store/OrderService.java
+++ src/main/java/zaki/example/store/OrderService.java
@@ -1,10 +1,14 @@
 package zaki.example.store;
 public class OrderService{
     private PaymentService paymentService;
+    public OrderService(){}
     public OrderService(PaymentService paymentService){
         this.paymentService=paymentService;
     }
     public void process(){
         paymentService.process(111);
+    }
+    public void setPaymentService(PaymentService paymentService){
+        this.paymentService=paymentService;
     }
 }
